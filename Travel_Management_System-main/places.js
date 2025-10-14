5913c6ee3025df27ea0f506c2f49de006bb09563
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2025-10-14 18:49:17 +0600

    Update places.js

--- Travel_Management_System-main/places.js
+++ Travel_Management_System-main/places.js
@@ -1,6 +1,7 @@
 const menuIcon = document.getElementById("menu-icon");
 const menu = document.getElementById("menu");
 
+
 menuIcon.addEventListener("click", () => {
 if (menu.className === "hidden") {
 menu.classList.remove("hidden");
